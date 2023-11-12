8e336dda7a3312084e67689468672fb9aa4bc484
bob <92489084+shadowdon66@users.noreply.github.com>
2023-11-11 20:56:22 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/GmPY29hw/Clown-Nose-Transparent.png');
+}
+
+function setup() {
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x+35;
+        noseY = results[0].pose.nose.y-25;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 400, 300);
+    image(clown_nose, noseX, noseY, 50, 50);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
